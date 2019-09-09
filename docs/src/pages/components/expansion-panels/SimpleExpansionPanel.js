05616896b367aa8d0a78433f488ee5d6d71550cc
François-Luc Haghenbeek <francois-luchaghenbeek@MacBook-Pro-de-Francois-Luc.local>
2019-09-09 17:05:52 +0200

    Format code with linting and prettier rules from MUI.

--- docs/src/pages/components/expansion-panels/SimpleExpansionPanel.js
+++ docs/src/pages/components/expansion-panels/SimpleExpansionPanel.js
@@ -24,7 +24,7 @@ export default function SimpleExpansionPanel() {
       <ExpansionPanel>
         <ExpansionPanelSummary
           expandIcon={<ExpandMoreIcon />}
-          expandIconPosition='left'
+          expandIconPosition="left"
           aria-controls="panel1a-content"
           id="panel1a-header"
         >
